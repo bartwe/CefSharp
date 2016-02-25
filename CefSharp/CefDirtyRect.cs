434d719b171e62ce3f56776b533181c3dd006323
bartwe <bluelive@gmail.com>
2016-02-25 03:15:43 +0100

    Be more sparse, and be correct in being sparse, and if the listener misses updates, provide the full dirtyRect.
    (cherry picked from commit 1b7f10f8910dfcb229c0841fa465f7b2e175d04c)

--- CefSharp/CefDirtyRect.cs
+++ CefSharp/CefDirtyRect.cs
@@ -2,17 +2,14 @@
 //
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 
-namespace CefSharp
-{
-    public struct CefDirtyRect
-    {
-        private readonly int x;
-        private readonly int y;
-        private readonly int width;
-        private readonly int height;
+namespace CefSharp {
+    public struct CefDirtyRect {
+        readonly int x;
+        readonly int y;
+        readonly int width;
+        readonly int height;
 
-        public CefDirtyRect(int x, int y, int width, int height)
-        {
+        public CefDirtyRect(int x, int y, int width, int height) {
             this.x = x;
             this.y = y;
             this.width = width;
@@ -25,13 +22,17 @@ namespace CefSharp
         public int Height { get { return height; } }
 
         public CefDirtyRect Combine(CefDirtyRect other) {
-            var lx = (x<other.x)?x:other.X;
-            var hx = ((x+width) > (other.x+other.width)) ? (x+width) : (other.x+other.width);
-        
+            if ((width == 0)||(height==0))
+                return other;
+            if ((other.width == 0)||(other.height == 0))
+                return this;
+            var lx = (x < other.x) ? x : other.X;
+            var hx = ((x + width) > (other.x + other.width)) ? (x + width) : (other.x + other.width);
+
             var ly = (y < other.y) ? y : other.y;
             var hy = ((y + height) > (other.y + other.height)) ? (y + height) : (other.y + other.height);
 
-            return new CefDirtyRect(lx, ly, hx-lx, hy-ly);
+            return new CefDirtyRect(lx, ly, hx - lx, hy - ly);
         }
     }
 }
